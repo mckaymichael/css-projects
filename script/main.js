e74232263e554137c34c297e632aff3a66b9f9e7
mckaymichael <mckaymichaelk@gmail.com>
2023-03-13 21:46:52 -0700

    added a welcome display

---
+++ script/main.js
@@ -0,0 +1,5 @@
+
+new TypeIt("#header", {
+    loop: true
+}).type("Welcome to My").break().type('CSS Website').pause(1000).delete().type('I create small').break().type('CSS Projects').break().type('For Fun').pause(1000).delete().type('enjoy :)').pause(5000).go();
+
